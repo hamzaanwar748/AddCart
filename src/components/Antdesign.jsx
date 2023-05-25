e361e2d66005b8a6be1ea3943de1cfbd9e43b221
hamzaanwar748 <123167841+hamzaanwar748@users.noreply.github.com>
2023-05-25 12:16:16 +0500

    added some move lines

--- src/components/Antdesign.jsx
+++ src/components/Antdesign.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Antdesign = () => {
-  return <div>kjsdafnk</div>;
+  return (
+    <div>
+      kjsdafnk
+      <div>slkfnd.m</div>
+    </div>
+  );
 };
 
 export default Antdesign;

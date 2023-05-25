2199af5c656688192ef5a0d9565858d61e57e553
hamzaanwar748 <123167841+hamzaanwar748@users.noreply.github.com>
2023-05-25 11:54:52 +0500

    all files commited

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import Cart from "./Cart";
-import Header from "./Header";
 import ProductDetails from "./ProductDetails";
 import Products from "./Products";
+import Antdesign from "./components/Antdesign";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Products />} />
-        <Route path="/ProductDetails/:productId" element={<ProductDetails />} />
-        <Route path="/Cart" element={<Cart />} />
-      </Routes>
-    </BrowserRouter>
+    <Antdesign />
+
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Products />} />
+    //     <Route path="/ProductDetails/:productId" element={<ProductDetails />} />
+    //     <Route path="/Cart" element={<Cart />} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
---
+++ src/components/Antdesign.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Antdesign = () => {
+  return <div>kjsdafnk</div>;
+};
+
+export default Antdesign;
